4509aea2ccec08fa84a36d0f28016004bd68a5d3
bohda <bohdan.konechniy@gmail.com>
2021-01-18 14:17:33 +0200

    Add: API methods

--- serverMiddleware/index.js
+++ serverMiddleware/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const app = require('express')();
 const cors = require('cors');
+const {extendWithUrls} = require('./helper/util');
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -15,6 +16,41 @@ function setupApi(data) {
         if (!getChildren) {
             delete data.children
         }
+
+        res.json({data: data})
+    })
+
+    app.get('/byPath', (req, res) => {
+        const rootData = JSON.parse(JSON.stringify(siteData));
+        const getChildren = req.query.getChildren || false;
+        const path = req.query.path;
+
+        let data = extendWithUrls({siteData: rootData, urlList}, path);
+
+        if (!getChildren) {
+            delete data.children
+        }
+
+        res.json({data: data})
+    })
+
+    app.get('/byContentType', (req, res) => {
+        const rootData = JSON.parse(JSON.stringify(siteData));
+        const contentType = req.query.contentType;
+        const getChildren = req.query.getChildren || false;
+        const data = {
+            nodeList: []
+        }
+
+        Object.values(rootData.children).forEach(item => {
+            if (item.ContentType === contentType) {
+                if (!getChildren) {
+                    delete item.children
+                }
+
+                data.nodeList.push(item)
+            }
+        })
 
         res.json({data: data})
     })
