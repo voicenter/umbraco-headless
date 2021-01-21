5a5558c18177c2b7a24f60f9834b0938d0f2630a
bohda <bohdan.konechniy@gmail.com>
2021-01-21 17:34:14 +0200

    Fix: empty route

--- router/index.js
+++ router/index.js
@@ -12,35 +12,33 @@ const setupRoutes = function (urlList) {
 
     this.extendRoutes(function umbracoModuleExtendRoutes(routes) {
         urlList.forEach(function (url) {
-            if (url.url === '' && url.TemplateAlias !== 'index') {
-                return;
+            if (url.url !== '' || url.TemplateAlias === 'index') {
+                let componentName = url.TemplateAlias + '.vue';
+
+                if (!files.includes(url.TemplateAlias + '.vue')) {
+                    console.warn('The ' + url.TemplateAlias + ' component is not created, redirecting the ' + url.url + ' route to the index.vue component.');
+
+                    componentName = 'index.vue';
+                }
+
+                const route = {
+                    name: url.nodeID,
+                    path: url.url,
+                    component: resolve('pages/' + componentName),
+                    meta: url
+                }
+
+                // Removes the automatically created routes by nuxt to prevent the duplicates
+                const duplicateIndex = routes.findIndex(function (route) {
+                    return route.name === url.TemplateAlias
+                })
+
+                if (duplicateIndex !== -1) {
+                    routes.splice(duplicateIndex, 1)
+                }
+
+                routes.push(route)
             }
-
-            let componentName = url.TemplateAlias + '.vue';
-
-            if (!files.includes(url.TemplateAlias + '.vue')) {
-                console.warn('The ' + url.TemplateAlias + ' component is not created, redirecting the ' + url.url + ' route to the index.vue component.');
-
-                componentName = 'index.vue';
-            }
-
-            const route = {
-                name: url.nodeID,
-                path: url.url,
-                component: resolve('pages/' + componentName),
-                meta: url
-            }
-
-            // Removes the automatically created routes by nuxt to prevent the duplicates
-            const duplicateIndex = routes.findIndex(function (route) {
-                return route.name === url.TemplateAlias
-            })
-
-            if (duplicateIndex !== -1) {
-                routes.splice(duplicateIndex, 1)
-            }
-
-            routes.push(route)
         })
     });
 }
