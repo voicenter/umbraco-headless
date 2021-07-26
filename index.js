a1380cb24834f612c76142c4fedc85b5297b1e8f
Bohdan <bohdan.ko@voicenter.com>
2021-07-26 12:39:45 +0300

    Add: trailing slash redirect

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import setupRoutes from './router';
 import setupApi from './api';
 import setupStore from './store';
 import setupPlugin from './plugins';
+import setupTrailingSashModule from './modules/trailing';
 
 export default function (moduleOptions) {
     const options = initOptions.call(this, moduleOptions);
@@ -11,6 +12,10 @@ export default function (moduleOptions) {
     setupApi.call(this, options);
     setupStore.call(this, options);
     setupPlugin.call(this, options);
+
+    if (moduleOptions.trailingSlashRedirect === true) {
+        setupTrailingSashModule.call(this, options)
+    }
 }
 
 module.exports.meta = require('./package.json');
